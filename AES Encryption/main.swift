103970c4859fb1533730e3b776992dfcce3a28fb
Aaron Michael Kippins <AKippins@hotmail.com>
2019-04-04 18:32:33 -0400

    Initial Commit

---
+++ AES Encryption/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AES Encryption
+//
+//  Created by Aaron Michael Kippins on 4/4/19.
+//  Copyright © 2019 Kippins. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
